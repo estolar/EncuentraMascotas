377ea9a1144be4e58db974364271d12e36233e33
Pamela Matias <pamelamc23@gmail.com>
2024-09-10 16:42:03 -0500

    cambios petlosts(new y edit)

--- app/controllers/petlosts_controller.rb
+++ app/controllers/petlosts_controller.rb
@@ -1,2 +1,24 @@
 class PetlostsController < ApplicationController
+  def new
+    @petlost = Petlost.new
+  end
+
+  def create
+    @petlost = Petlost.new(petlost_params)
+    if @petlost.save
+      redirect_to service_path(@petlost)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @petlost = Petlost.find(params[:id])
+  end
+
+  def update
+    @petlost = Petlost.find(params[:id])
+    @petlost.update(petlost_params)
+    redirect_to petlost_path(@petlost)
+  end
 end
