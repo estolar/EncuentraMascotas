d1ec6e63101983a66778e86292e3e385cd33af27
gslazar96@gmail.com <gslazar96@gmail.com>
2024-09-10 17:32:06 -0500

    Metodos Terminados INDEX SHOW DESTROY

--- app/controllers/petlosts_controller.rb
+++ app/controllers/petlosts_controller.rb
@@ -10,6 +10,6 @@ class PetlostsController < ApplicationController
   def destroy
     @petlost = Petlost.find(params[:id])
     @petlost.destroy
-    redirect_to petlosts_path
+    redirect_to petlosts_path, status: :see_other
   end
 end
