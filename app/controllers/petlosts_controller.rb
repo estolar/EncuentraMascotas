834012f9e4e65097d5e62280de35c3d3294b0e0f
gslazar96@gmail.com <gslazar96@gmail.com>
2024-09-10 16:58:34 -0500

    Se termina Index

--- app/controllers/petlosts_controller.rb
+++ app/controllers/petlosts_controller.rb
@@ -1,2 +1,15 @@
 class PetlostsController < ApplicationController
+  def index
+    @petlosts = Petlost.all
+  end
+
+  def show
+    @petlost = Petlost.find(params[:id])
+  end
+
+  def destroy
+    @petlost = Petlost.find(params[:id])
+    @petlost.destroy
+    redirect_to petlosts_path
+  end
 end
