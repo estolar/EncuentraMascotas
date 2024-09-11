0e69b175fa1f27397c287f6331059739c5a94401
gslazar96@gmail.com <gslazar96@gmail.com>
2024-09-11 14:55:55 -0500

    Mapas terminados ojo muchos cambios

--- app/models/petlost.rb
+++ app/models/petlost.rb
@@ -1,4 +1,6 @@
 class Petlost < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
   BREEDS = ['Labrador Retriever','Golden Retriever','Pastor Alemán','Shih Tzu','Beagle',
     'Bulldog Francés','Rottweiler','Chihuahua', 'Schnauzer', 'Pug', 'Dóberman', 'Cocker Spaniel',
