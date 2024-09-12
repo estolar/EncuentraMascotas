10a67c71ffde917894c515740ab8ad46f760dcd5
Pamela Matias <pamelamc23@gmail.com>
2024-09-12 16:34:05 -0500

    new-edit-petfound

--- app/controllers/petfounds_controller.rb
+++ app/controllers/petfounds_controller.rb
@@ -1,2 +1,30 @@
 class PetfoundsController < ApplicationController
+  def new
+    @petfound = Petfound.new
+  end
+
+  def create
+    @petfound = Petfound.new(petfound_params)
+    if @petfound.save
+      redirect_to petfound_path(@petfound)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @petfound = Petfound.find(params[:id])
+  end
+
+  def update
+    @petfound = Petfound.find(params[:id])
+    @petfound.update(petfound_params)
+    redirect_to petfound_path(@petfound)
+  end
+
+  private
+
+  def petfound_params
+    params.require(:petfound).permit(:breed, :color, :signs, :details, :day_found, :user_id, photos: [])
+  end
 end
