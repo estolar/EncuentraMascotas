4edb48d9b72d794f69ec272167792400eea2cf10
Pamela Matias <pamelamc23@gmail.com>
2024-09-13 16:02:13 -0500

    cambios frontend

--- db/schema.rb
+++ db/schema.rb
@@ -70,10 +70,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_175118) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "facts"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.string "facts"
     t.index ["user_id"], name: "index_petfounds_on_user_id"
   end
 
