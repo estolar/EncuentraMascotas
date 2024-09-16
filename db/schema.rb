cce8f7f9f395d4a0bfa95869b2faf78c7e2ae938
Victor Huatuco <victorhuatucov@gmail.com>
2024-09-16 17:20:44 -0500

    number and name - users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_13_175118) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_16_173712) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,10 +70,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_13_175118) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "facts"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.string "facts"
     t.index ["user_id"], name: "index_petfounds_on_user_id"
   end
 
