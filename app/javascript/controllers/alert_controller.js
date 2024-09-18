18080060555e306ec8b4b4896ab56265a2f49c8a
Pamela Matias <pamelamc23@gmail.com>
2024-09-18 14:01:57 -0500

    cambios alertas sweetalert

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,23 +1,55 @@
 import { Controller } from "@hotwired/stimulus"
 import Swal from 'sweetalert2';
 
-// Connects to data-controller="alert"
 export default class extends Controller {
   connect() {
     console.log("alertas conectado!")
   }
   showAlert(event) {
-    event.preventDefault(); // Detenemos el envío del formulario
+    event.preventDefault();
+    console.log('turbo end conectado')
+    // console.log(event.detail)
+    console.log(event.detail.success)
+    // console.log(event.detail.formSubmission)
+    console.log(event.detail.formSubmission.result.success)
+    console.log(Swal)
+    // event.preventDefault();
+    const mode = this.element.dataset.mode;
+    let title, text;
+    if (mode === "new") {
+      title = "¡Registro exitoso!";
+      text = "Esperamos tener pronto buenas noticias para ti";
+    } else if (mode === "edit") {
+      title = "¡Registro actualizado!";
+      text = "Has actualizado la información de tu mascotita correctamente.";
+    }
 
-    Swal.fire({
-      title: '¡Registro actualizado!',
-      text: 'Esperamos ayudarte a encontrarlo(a) pronto',
-      icon: 'success',
-      timer: 2500,
-      timerProgressBar: true,
-      showConfirmButton: false
-    }).then(() => {
-      this.element.submit();
-    });
+
+    if (event.detail.formSubmission.result.success) {
+      console.log("hecho!");
+      Swal.fire({
+        title: 'Success',
+        text: 'You are connected',
+        icon: 'success',
+        timer: 5000,
+        timerProgressBar: true
+      }).then(() => {
+        // Redirige después de que la alerta se ha mostrado
+        this.element.submit();
+      });
+    } else {
+      console.log("error");
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>',
+        timer: 5000,
+        timerProgressBar: true
+      }).then(() => {
+        // Redirige o maneja el error después de que la alerta se ha mostrado
+        this.element.submit();
+      });
+    }
   }
 }
