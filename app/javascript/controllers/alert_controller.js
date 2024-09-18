3b5d889bb151df9ea0047ec9d97dc69766e93785
Pamela Matias <pamelamc23@gmail.com>
2024-09-18 15:03:07 -0500

    alerta success y boton eliminar

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -9,12 +9,6 @@ export default class extends Controller {
 
     event.preventDefault();
     console.log('turbo end conectado')
-    // console.log(event.detail)
-    // console.log(event.detail.success)
-    // console.log(event.detail.formSubmission)
-    // console.log(event.detail.formSubmission.result.success)
-    // console.log(Swal)
-    // event.preventDefault();
     const mode = this.element.dataset.mode;
     let title, text;
     if (mode === "new") {
@@ -24,34 +18,6 @@ export default class extends Controller {
       title = "¡Registro actualizado!";
       text = "Has actualizado la información de tu mascotita correctamente.";
     }
-
-
-    // if (event.detail.formSubmission.result.success) {
-    //   console.log("hecho!");
-    //   Swal.fire({
-    //     title: 'Success',
-    //     text: 'You are connected',
-    //     icon: 'success',
-    //     timer: 5000,
-    //     timerProgressBar: true
-    //   }).then(() => {
-    //     // Redirige después de que la alerta se ha mostrado
-    //     this.element.submit();
-    //   });
-    // } else {
-    //   console.log("error");
-    //   Swal.fire({
-    //     icon: "error",
-    //     title: "Oops...",
-    //     text: "Something went wrong!",
-    //     footer: '<a href="#">Why do I have this issue?</a>',
-    //     timer: 5000,
-    //     timerProgressBar: true
-    //   }).then(() => {
-    //     // Redirige o maneja el error después de que la alerta se ha mostrado
-    //     this.element.submit();
-    //   });
-    // }
 
     const swalOptions = {
       title: title,
@@ -68,20 +34,6 @@ export default class extends Controller {
         // Redirige después de que la alerta se ha mostrado
         this.element.submit();
       });
-    // } else {
-    //   console.log("error");
-    //   Swal.fire({
-    //     icon: "error",
-    //     title: "Oops...",
-    //     text: "Something went wrong!",
-    //     footer: '<a href="#">Why do I have this issue?</a>',
-    //     timer: 5000,
-    //     timerProgressBar: true,
-    //     showConfirmButton: false
-    //   }).then(() => {
-    //     // Redirige después de que la alerta se ha mostrado
-    //     this.element.submit();
-    //   });
-    // }
+
   }
 }
